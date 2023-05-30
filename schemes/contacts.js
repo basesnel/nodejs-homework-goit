9c28fded53e2bd6077da5db9c8bf218acad44e05
basesnel <basesnel@gmail.com>
2023-05-30 15:18:50 +0300

    Homework: warnings renamed to messages in validation

--- schemes/contacts.js
+++ schemes/contacts.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-const warnings = {
+const messages = {
   required: "missing required {{#label}} field",
   noPattern: "invalid {{#label}}, must be",
   empty: "{{#label}} cannot be an empty field",
@@ -8,24 +8,24 @@ const warnings = {
 
 const contactAddScheme = Joi.object({
   name: Joi.string().required().messages({
-    "any.required": warnings.required,
-    "string.empty": warnings.empty,
+    "any.required": messages.required,
+    "string.empty": messages.empty,
   }),
   email: Joi.string()
     .required()
     .pattern(new RegExp(/\S+@\S+\.\S+/))
     .messages({
-      "string.pattern.base": `${warnings.noPattern} (i.e): example@domain.tld`,
-      "any.required": warnings.required,
-      "string.empty": warnings.empty,
+      "string.pattern.base": `${messages.noPattern} (i.e): example@domain.tld`,
+      "any.required": messages.required,
+      "string.empty": messages.empty,
     }),
   phone: Joi.string()
     .required()
     .pattern(new RegExp(/[(]?\d{3}[)]? \d{3}-\d{4}/))
     .messages({
-      "string.pattern.base": `${warnings.noPattern}: (000) 000-0000`,
-      "any.required": warnings.required,
-      "string.empty": warnings.empty,
+      "string.pattern.base": `${messages.noPattern}: (000) 000-0000`,
+      "any.required": messages.required,
+      "string.empty": messages.empty,
     }),
 });
 
